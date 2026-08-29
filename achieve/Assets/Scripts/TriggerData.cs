806d47a263c261b998b950ecdf61301f0727a7d5
Hallo Tallo <dandura13@protonmail.com>
2026-08-28 22:45:19 -0500

    added wip triggerdata

--- achieve/Assets/Scripts/TriggerData.cs
+++ achieve/Assets/Scripts/TriggerData.cs
@@ -1,7 +1,65 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 //a place to store all of the triggers used by the game's achievements
-public class TriggerData
+public static class TriggerData
 {
-    
+    //private static List<Trigger> ALL_TRIGGERS = new List<Trigger>();
+    //static Trigger a = new Trigger("a");
+    //Debug.Log(a.GetName());
+    //ALL_TRIGGERS.Add(a);
+    /*ALL_TRIGGERS.Add(new Trigger("MoveDown"));
+    ALL_TRIGGERS.Add(new Trigger("MoveLeft"));
+    ALL_TRIGGERS.Add(new Trigger("MoveRight"));
+    ALL_TRIGGERS.Add(new Trigger("Squawk"));
+    ALL_TRIGGERS.Add(new Trigger("Squawk5"));
+    ALL_TRIGGERS.Add(new Trigger("Squawk50"));
+    ALL_TRIGGERS.Add(new Trigger("Die"));
+    ALL_TRIGGERS.Add(new Trigger("Die5"));
+    ALL_TRIGGERS.Add(new Trigger("Die42"));
+    ALL_TRIGGERS.Add(new Trigger("DieFast"));
+    ALL_TRIGGERS.Add(new Trigger("KonamiCode"));
+    ALL_TRIGGERS.Add(new Trigger("Alive10s"));
+    ALL_TRIGGERS.Add(new Trigger("Alive30s"));
+    ALL_TRIGGERS.Add(new Trigger("Alive300s"));
+    ALL_TRIGGERS.Add(new Trigger("Key1"));
+    ALL_TRIGGERS.Add(new Trigger("Key2"));
+    ALL_TRIGGERS.Add(new Trigger("Key3"));
+    ALL_TRIGGERS.Add(new Trigger("Key4"));
+    ALL_TRIGGERS.Add(new Trigger("Key5"));
+    ALL_TRIGGERS.Add(new Trigger("Key6"));
+    ALL_TRIGGERS.Add(new Trigger("Key7"));
+    ALL_TRIGGERS.Add(new Trigger("Key8"));
+    ALL_TRIGGERS.Add(new Trigger("Key9"));
+    ALL_TRIGGERS.Add(new Trigger("Key0"));
+    ALL_TRIGGERS.Add(new Trigger("Pause"));
+    ALL_TRIGGERS.Add(new Trigger("Unpause"));
+    ALL_TRIGGERS.Add(new Trigger("Fall"));
+    ALL_TRIGGERS.Add(new Trigger("NoSquawk60"));
+    ALL_TRIGGERS.Add(new Trigger("CollideTree"));
+    ALL_TRIGGERS.Add(new Trigger("DecreaseAudio"));
+    ALL_TRIGGERS.Add(new Trigger("MuteAudio"));
+    ALL_TRIGGERS.Add(new Trigger("1Ach"));
+    ALL_TRIGGERS.Add(new Trigger("10Ach"));
+    ALL_TRIGGERS.Add(new Trigger("50Ach"));
+    ALL_TRIGGERS.Add(new Trigger("Click"));
+    ALL_TRIGGERS.Add(new Trigger("RightClick"));
+    ALL_TRIGGERS.Add(new Trigger("MiddleClick"));
+    ALL_TRIGGERS.Add(new Trigger("SecretSpot"));
+    ALL_TRIGGERS.Add(new Trigger("FlowerRing"));
+    ALL_TRIGGERS.Add(new Trigger("Flytrap"));
+    ALL_TRIGGERS.Add(new Trigger("Flytrap2"));
+    ALL_TRIGGERS.Add(new Trigger("Flytrap3"));
+    ALL_TRIGGERS.Add(new Trigger("Flytrap4"));
+    ALL_TRIGGERS.Add(new Trigger("Flytrap5"));
+    ALL_TRIGGERS.Add(new Trigger("Settings"));
+    ALL_TRIGGERS.Add(new Trigger("Credits"));
+    ALL_TRIGGERS.Add(new Trigger("TrampleFlower"));
+    ALL_TRIGGERS.Add(new Trigger("TrampleFlower5"));
+    ALL_TRIGGERS.Add(new Trigger("TrampleFlowerAll"));
+
+    public static Trigger GetTrigger(string name){
+        return ALL_TRIGGERS.Find(i => i.GetName() == name)
+    }
+    */
 }
